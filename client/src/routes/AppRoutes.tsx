e9a6671e09a10b118d16cd7b6e7063b49ac177d9
MariaCrystala <ailagarganza@gmail.com>
2026-04-07 02:37:42 +0800

    Part 6 Done - Select and Properties

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,13 +1,37 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
 import FloatingLabelInput from "../components/Input/FloatingLabelInput";
+import FloatingLabelSelect from "../components/Select/FloatingLabelSelect";
 import { useState } from "react";
+import { Table, TableBody, TableCell, TableHeader, TableRow } from "../components/Table";
 
 const SampleComponent = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [birthDate, setBirthDate] = useState("");
   const [password, setPassword] = useState("");
+  const [gender, setGender] = useState("");
+
+
+  const genders = [
+    {
+      value: "",
+      text: "Select Gender",
+    },
+    {
+      value: 1,
+      text: "Male",
+    },
+    {
+      value: 2,
+      text: "Female",
+    },
+    {
+      value: 3,
+      text: "Prefer Not to Say",
+    }
+  ];
+
   return (
     <>
       <h1 className="text-red-600 mb-4">Hello World</h1>
@@ -56,6 +80,39 @@ const SampleComponent = () => {
           required
         />
         <p className="font-medium">Password: {password}</p>
+      </div>
+      <div className="mb-4">
+        <FloatingLabelSelect 
+          label="Gender" 
+          name="gender" 
+          value={gender} 
+          onChange={(e) => setGender(e.target.value)} 
+        >
+          {genders.map((gender, index)=>(
+            <option value={gender.value} key={index}>{gender.text} </option>
+          ))}
+        </FloatingLabelSelect>
+        <p className="font-medium">Gender: {gender}</p>
+      </div>
+      <div className="overflow-hidden rounded-lg border border-gray-200 bg-white">
+        <div className="max-w-full max-h-[calc(100vh)] overflow-x-auto">
+          <Table>
+            <TableHeader className="border-b border-gray-100 bg-blue-500 text-white sticky top-0 z-30 text-xs">
+              <TableRow>
+                <TableCell isHeader className="px-5 py-3 font-medium text-center">No.</TableCell>
+                <TableCell isHeader className="px-5 py-3 font-medium text-center">Gender</TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {genders.map((gender, index) => (
+                <TableRow key={index}>
+                  <TableCell className="px-4 py-3 text-center"> {gender.value} </TableCell>
+                  <TableCell className="px-4 py-3 text-center"> {gender.text} </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </>
   );
